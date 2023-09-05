9a60bf3d34a04d1dccf240250fc97a1168e31a3c
kannan742003 <kannan742003@gmail.com>
2023-09-05 17:44:46 +0530

    name is changed

--- s3-notification-triggers.sh
+++ s3-notification-triggers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -e
 
 # Store the AWS account ID in a variable
 aws_account_id=$(aws sts get-caller-identity --query 'Account' --output text)
@@ -10,13 +10,13 @@ echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
-lambda_func_name="s3-lambda-function"
-role_name="s3-lambda-sns"
+bucket_name="kanna-ultimate-bucket"
+lambda_func_name="lambda-function"
+role_name="simplee-lambda-sns"
 email_address="kannan742003@gmail.com"
 
 # Create IAM Role for the project
-role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
+role_response=$(aws iam create-role --role-name $role_name --assume-role-policy-document '{
   "Version": "2012-10-17",
   "Statement": [{
     "Action": "sts:AssumeRole",
@@ -41,29 +41,24 @@ echo "Role ARN: $role_arn"
 aws iam attach-role-policy --role-name $role_name --policy-arn arn:aws:iam::aws:policy/AWSLambda_FullAccess
 aws iam attach-role-policy --role-name $role_name --policy-arn arn:aws:iam::aws:policy/AmazonSNSFullAccess
 
-# Create the S3 bucket and capture the output in a variable
-bucket_output=$(aws s3api create-bucket --bucket "$bucket_name" --region "$aws_region")
-
-# Print the output from the variable
-echo "Bucket creation output: $bucket_output"
+# Create the S3 bucket
+aws s3api create-bucket --bucket "$bucket_name" --region "$aws_region"
 
 # Upload a file to the bucket
-aws s3 cp ./example_file.txt s3://"$bucket_name"/example_file.txt
+aws s3 cp /C:/Users/Client/shellproject/shellscriptingproject/example_file.txt s3://"$bucket_name"/example_file.txt
 
 # Create a Zip file to upload Lambda Function
-zip -r s3-lambda-function.zip ./s3-lambda-function
+#zip -r lambda-function.zip /C:/Users/Client/shellproject/shellscriptingproject/lambda-function
 
-sleep 5
 # Create a Lambda function
 aws lambda create-function \
   --region "$aws_region" \
   --function-name $lambda_func_name \
-  --runtime "python3.8" \
-  --handler "s3-lambda-function/s3-lambda-function.lambda_handler" \
+  --runtime "python3.11" \
+  --handler "lambda-function.lambda_handler" \
   --memory-size 128 \
   --timeout 30 \
-  --role "arn:aws:iam::$aws_account_id:role/$role_name" \
-  --zip-file "fileb://./s3-lambda-function.zip"
+  --role "$role_arn" \
 
 # Add Permissions to S3 Bucket to invoke Lambda
 aws lambda add-permission \
@@ -74,7 +69,7 @@ aws lambda add-permission \
   --source-arn "arn:aws:s3:::$bucket_name"
 
 # Create an S3 event trigger for the Lambda function
-LambdaFunctionArn="arn:aws:lambda:us-east-1:$aws_account_id:function:s3-lambda-function"
+LambdaFunctionArn="arn:aws:lambda:$aws_region:$aws_account_id:function:$lambda_func_name"
 aws s3api put-bucket-notification-configuration \
   --region "$aws_region" \
   --bucket "$bucket_name" \
@@ -86,24 +81,27 @@ aws s3api put-bucket-notification-configuration \
 }'
 
 # Create an SNS topic and save the topic ARN to a variable
-topic_arn=$(aws sns create-topic --name s3-lambda-sns --output json | jq -r '.TopicArn')
+topic_arn=$(aws sns create-topic --name simplee-lambda-sns --output json | jq -r '.TopicArn')
 
 # Print the TopicArn
 echo "SNS Topic ARN: $topic_arn"
 
-# Trigger SNS Topic using Lambda Function
-
-
 # Add SNS publish permission to the Lambda Function
-aws sns subscribe \
-  --topic-arn "$topic_arn" \
-  --protocol email \
-  --notification-endpoint "$email_address"
+aws lambda add-permission \
+  --function-name "$lambda_func_name" \
+  --statement-id "sns-lambda-permission" \
+  --action "lambda:InvokeFunction" \
+  --principal sns.amazonaws.com \
+  --source-arn "$topic_arn"
 
 # Publish SNS
 aws sns publish \
   --topic-arn "$topic_arn" \
-  --subject "A new object created in s3 bucket" \
+  --subject "A new object created in S3 bucket" \
   --message "Hello from Abhishek.Veeramalla YouTube channel, Learn DevOps Zero to Hero for Free"
+
+echo "Script executed successfully"
+
+
 
 
